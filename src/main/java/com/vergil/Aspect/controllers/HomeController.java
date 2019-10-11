2fc06740b8c758ead428733bca9a1f21115f9aab
Virgilio <virgiliosps@gmail.com>
2019-10-11 00:12:58 -0300

    Primeira tentantiva de fazer o aspect funcionar, não funcionou

--- src/main/java/com/vergil/Aspect/controllers/HomeController.java
+++ src/main/java/com/vergil/Aspect/controllers/HomeController.java
@@ -1,13 +1,19 @@
-package com.vergil.Aspect.controller;
+package com.vergil.Aspect.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String home(){
+        log.debug("ENtrou nessa porra");
         return "Hello World!";
     }
 }
