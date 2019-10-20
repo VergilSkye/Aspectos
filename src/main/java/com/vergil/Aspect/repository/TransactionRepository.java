9b67f5222498913a01d248750040d22bb2761d34
Virgilio <virgiliosps@gmail.com>
2019-10-20 00:23:11 -0300

    feature: novos metodos

--- src/main/java/com/vergil/Aspect/repository/TransactionRepository.java
+++ src/main/java/com/vergil/Aspect/repository/TransactionRepository.java
@@ -2,8 +2,17 @@ package com.vergil.Aspect.repository;
 
 import com.vergil.Aspect.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("select a from Transaction a where a.account.id =(?1)")
+    List<Transaction> buscarTransacationPorAccountId(Long id);
+
+    @Query("select a from Transaction a where a.account.user.id =(?1)")
+    List<Transaction> buscarTransacationPorUserId(Long id);
 }
